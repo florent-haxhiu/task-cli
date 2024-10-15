276e29c9efce6111d985b631ab0b4775ee1565cb
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-10-15 21:49:55 +0100

    add: use helper func to make the default the free id
    Had to wrap the helper func in another func and return it so it doesn't
    throw any errors when trying to put it in the code, probably a nicer way
    to do this

--- src/commands/task.rs
+++ src/commands/task.rs
@@ -1,9 +1,18 @@
 use clap::Parser;
+use rusqlite::Connection;
+use task_cli::get_free_id;
+
+use crate::table::table::connect_to_db;
+
+fn get_id() -> i32 {
+    let conn: Connection = connect_to_db().unwrap();
+    get_free_id(&conn).unwrap()
+}
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
 pub struct Task {
-    #[arg(short, long, default_value_t = 1)]
+    #[arg(short, long, default_value_t = get_id())]
     pub id: i32,
     #[arg(short, long)]
     pub name: String,
