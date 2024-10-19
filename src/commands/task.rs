5f9794984eca65bc17a150ed09eee62ba7b75089
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-10-19 11:18:14 +0100

    change: no need to include all the extra on the struct

--- src/commands/task.rs
+++ src/commands/task.rs
@@ -1,21 +1,6 @@
-use clap::Parser;
-use rusqlite::Connection;
-use task_cli::get_free_id;
-
-use crate::table::table::connect_to_db;
-
-fn get_id() -> i32 {
-    let conn: Connection = connect_to_db().unwrap();
-    get_free_id(&conn).unwrap()
-}
-
-#[derive(Parser, Debug, Clone)]
-#[command(version, about, long_about = None)]
+#[derive(Debug, Clone)]
 pub struct Task {
-    #[arg(short, long, default_value_t = get_id())]
     pub id: i32,
-    #[arg(short, long)]
     pub name: String,
-    #[arg(short, long, default_value_t = false)]
     pub done: bool,
 }
