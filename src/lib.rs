1cdbf7f45836a9f6fbfe7ae5c7a01d0d02bd66f5
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-10-19 11:16:56 +0100

    change: had to look for the max id then increment by one

--- src/lib.rs
+++ src/lib.rs
@@ -7,24 +7,23 @@ struct TaskId {
 
 // Get tasks and see which id is free
 pub fn get_free_id(conn: &Connection) -> Result<i32, Error> {
-    let mut stmt = conn.prepare("SELECT id FROM tasks")?;
+    let mut stmt = conn.prepare("SELECT MAX(id) AS max_id FROM tasks")?;
+    let mut free_id: i32 = 0;
 
     let id_iter = stmt.query_map([], |row| Ok(TaskId { id: row.get(0)? }))?;
-
-    let mut free_id: i32 = 0;
 
     for id in id_iter {
         match id {
             Ok(id) => {
                 if free_id <= id.id {
-                    free_id += 1
+                    free_id = id.id + 1
                 }
             }
             _ => {
-                println!("Error");
+                eprintln!("id not here")
             }
         }
     }
 
-    return Ok(free_id);
+    Ok(free_id)
 }
