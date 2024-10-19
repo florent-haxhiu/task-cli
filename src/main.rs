c61a4ef85c911874b2fa04e41dcb7907ffd7dc8d
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-10-19 09:13:26 +0100

    add: add functionality works

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,19 @@ fn main() -> Result<()> {
     let all_tasks = show_all_tasks(&conn);
     println!("{:#?}", all_tasks);
 
+    match matches.subcommand() {
+        Some(("add", sub_matches)) => {
+            println!("Adding task");
+            println!("{:#?}", sub_matches);
+            let name = sub_matches.get_one::<String>("NAME").expect("required");
+            let task = Task {
+                id: get_free_id(&conn)?,
+                name: name.to_string(),
+                done: false,
+            };
+            let _ = add_task(&conn, &task);
+        }
+        }
     }
     Ok(())
 }
