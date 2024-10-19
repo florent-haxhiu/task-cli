6eecb7beb6f70233282f60c67dcb9f131256b0e5
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-10-19 12:43:57 +0100

    add: formatting & Eq for testing

--- src/commands/task.rs
+++ src/commands/task.rs
@@ -1,6 +1,14 @@
-#[derive(Debug, Clone)]
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Task {
     pub id: i32,
     pub name: String,
     pub done: bool,
+}
+
+impl Display for Task {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {:.3}: {}", self.id, self.name, self.done)
+    }
 }
